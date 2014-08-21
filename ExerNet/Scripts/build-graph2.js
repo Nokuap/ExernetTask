85c35b038fa55aeacdb59b2802068638974f84ba
Nokuap <pauk1993-2008@mail.ru>
2014-08-21 18:59:43 +0300

    asdf

--- ExerNet/Scripts/build-graph2.js
+++ ExerNet/Scripts/build-graph2.js
@@ -1,8 +1,9 @@
 ﻿$(document).ready(function () {
     var expr = Parser.parse($("#expression").val());
     var cosPoints = [];
-
-    for (var i = parseFloat($("#from").val()) ; i < $("#to").val() ; i += parseFloat($("#step").val())) {
+    var increment = parseFloat($("#step").val());
+    if (increment == 0) increment = 0.005;
+    for (var i = parseFloat($("#from").val()) ; i < $("#to").val() ; i += increment) {
         cosPoints.push([i, expr.evaluate({ x: i })]);
     }
     var plot1 = $.jqplot('chart1', [cosPoints], {
